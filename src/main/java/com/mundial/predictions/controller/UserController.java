0d6003d6b7cd33809fbe6132167decd127230bfa
PRVirkrys <pala.3633@gmail.com>
2026-06-05 17:29:24 +0200

    feat: add get user by id endpoint

--- src/main/java/com/mundial/predictions/controller/UserController.java
+++ src/main/java/com/mundial/predictions/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,6 +49,11 @@ public class UserController {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable Integer id) {
+		return userService.findUserById(id);
 	}
 
 }
